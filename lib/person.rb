4860342f20c21773b3087fcc23c3fe3738703463
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-25 21:00:48 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,11 +3,11 @@ class Person
     @name = name
   end
  
-  def breed=(breed)
-    @breed = breed
+  def name=(name)
+    @name = name
   end
  
-  def breed
-    @breed
+  def name
+    @name
   end
 end
